733d84eb81ef10e8d8bc6e2d5836bad6e79c3d3b
Hargaaya <hargaaya10@gmail.com>
2023-03-12 22:03:54 +0100

    add basic object persistance layer

---
+++ IAmAGame-Backend/Persistance/RedisDatabase.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace IAmAGame_Backend.Persistance;
+
+public class RedisDatabase
+{
+    private IDatabase db;
+
+    public RedisDatabase()
+    {
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        db = redis.GetDatabase();
+    }
+
+    public void Set<T>(string key, T data)
+    {
+        string value = JsonSerializer.Serialize(data);
+        db.StringSet(key, value);
+    }
+
+    public T? Get<T>(string key)
+    {
+        string? value = db.StringGet(key);
+        if (value == null) return default;
+        T? data = JsonSerializer.Deserialize<T>(value);
+
+        return data;
+    }
+
+}
