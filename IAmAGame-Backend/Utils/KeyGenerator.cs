182d9d0d4c72b8a0962aa528a30afe7aaed0ad9e
Hargaaya <hargaaya10@gmail.com>
2023-04-09 22:31:15 +0200

    add room key generator

---
+++ IAmAGame-Backend/Utils/KeyGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace IAmAGame_Backend.Utils;
+
+public class KeyGenerator
+{
+    public string GenerateRoomKey()
+    {
+        string key = "";
+        Random random = new Random();
+        for (int i = 0; i < 5; i++)
+        {
+            int randomInt = random.Next(0, 2);
+            if (randomInt == 0)
+            {
+                key += (char)random.Next(65, 91);
+            }
+            else
+            {
+                key += (char)random.Next(97, 123);
+            }
+        }
+        return key;
+    }
+}
+
