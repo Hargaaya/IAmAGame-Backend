1aa9ddd453987230eda723f8a2fad27ff2b78309
Hargaaya <hargaaya10@gmail.com>
2023-05-07 21:09:03 +0200

    add interface, add error handling

--- IAmAGame-Backend/Persistance/RedisDatabase.cs
+++ IAmAGame-Backend/Persistance/RedisDatabase.cs
@@ -3,29 +3,34 @@ using System.Text.Json;
 
 namespace IAmAGame_Backend.Persistance;
 
-public class RedisDatabase
+public class RedisDatabase : IRedisDatabase
 {
-  private IDatabase db;
+    private IDatabase db;
 
-  public RedisDatabase()
-  {
-    ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-    db = redis.GetDatabase();
-  }
+    public RedisDatabase()
+    {
+        // TODO: Move out db connection string to a config file
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        db = redis.GetDatabase();
+    }
 
-  public void Set<T>(string key, T data)
-  {
-    string value = JsonSerializer.Serialize(data);
-    db.StringSet(key, value);
-  }
+    public void Set<T>(string key, T data)
+    {
+        string value = JsonSerializer.Serialize(data);
+        db.StringSet(key, value);
+    }
 
-  public T? Get<T>(string key)
-  {
-    string? value = db.StringGet(key);
-    if (value == null) return default;
-    T? data = JsonSerializer.Deserialize<T>(value);
+    public T Get<T>(string key)
+    {
+        string nullMessage = $"Data for game with key: {key}, is null";
 
-    return data;
-  }
+        string? value = db.StringGet(key);
+        _ = value ?? throw new NullReferenceException(nullMessage);
+
+        T? data = JsonSerializer.Deserialize<T>(value);
+        _ = data ?? throw new NullReferenceException(nullMessage);
+
+        return data;
+    }
 
 }
